ceaac17a82bbe87ac09567ae7f9ec31ff6c2f7b7
NobleRhed <pod_routine_06@icloud.com>
2024-02-06 09:19:57 -0500

    Reorg - and fixes

---
+++ Week4Practice/src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        arrayExample();
+    }
+
+public static void arrayExample() {
+    int[] arrayNum = {-10, 3, 27, 8, -34, 76, 1, 12};
+    int sum = 0;
+    double average = 0;
+
+    for (int num:arrayNum) {
+        sum = sum + num;
+    }
+
+    average = (double) sum/arrayNum.length;
+    //double is needed to make the exact value display, as INT has no decimal
+
+    System.out.println("The sum is - "+ sum + " and the average is - " + average);
+    }
+}
